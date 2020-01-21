a5175873ce078be6f6ab76a2f30d029cd1e71ca7
DaraKing <dario.knezovich@gmail.com>
2020-01-21 22:12:36 +0100

    pushed what we done in goranci

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ var express = require("express"),
     app = express(),
     port = process.env.PORT || 3000;
 
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
-import * as u from 'public/js/dist/utilis.js';
-import * as _c from 'public/js/dist/constants.js';
+
+// const _c = import('public/js/dist/constants.js')
+//     .catch((err) => {
+//         console.log(err)
+//     });
+// const u = import('public/js/dist/utilis.js')
+//     .catch((err) => {
+//         console.log(err)
+//     });
 
 //=========
 //ROUTES
@@ -55,27 +63,61 @@ app.get("/photocon/food/new", function(req, res){
     res.render("newFood");
 });
 
+function apiCall(method = 'GET', url, data) {
+
+    let payload;
+
+    let xhr = new XMLHttpRequest();
+    xhr.open(method, url, false);
+    xhr.setRequestHeader('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImRhcmlvLmtuZXpvdmljaEBnbWFpbC5jb20iLCJ1c2VyX2lkIjoxLCJyb2xlX2lkIjozLCJpYXQiOjE1NzkyOTcxMzV9.1SDu5PBgRP-kvVZzGuZQstKIJA6v4pR0Ev7iI4jTtak');
+    xhr.onload  = function () {
+        if (this.status >= 200 && this.status < 300) {
+            payload = JSON.parse(xhr.responseText);
+        } else {
+            console.warn({
+                status    : this.status,
+                statusText: xhr.statusText,
+                error     : this.responseText,
+            });
+        }
+    };
+    xhr.onerror = function () {
+        console.error({
+            status    : this.status,
+            statusText: xhr.statusText,
+        });
+    };
+    xhr.send(data);
+
+    return payload;
+}
 
 //OVDE CE ICI ZA LOGIN I SIGN UP
 
-
-
 //USERS ROUTES
 app.get("/users", function(req, res){
-    let users;
-    u._apiCall('GET', `${_c.API_URL.user}/api/users/all`)
-        .then((resp) => {
-            users = resp;
-        })
-        .catch((error) => {
-            console.log(error)
-        });
+    let users = apiCall('GET' ,'http://localhost:4000/api/users/all',null);
+    if (!users) {
+        return
+    }
+
     res.render("users", {data: users});
 });
 
 //USER EDIT
-app.get("/users/edit", function(req, res){
-    res.render("userEdit");
+app.get("/users/edit/:id", function(req, res){
+    let id = req.params.id;
+    let user = apiCall('GET', `http://localhost:4000/api/users/${id}`, null);
+    if(!user) {
+        return
+    }
+
+    let roles = apiCall('GET', 'http://localhost:4000/api/roles/all', null);
+    if(!roles) {
+        return
+    }
+
+    res.render("userEdit", {user, roles});
 });
 
 //LISTENER
