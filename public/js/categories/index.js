ea87e7938d57a6c5a03e09103b0880e767eefd16
DaraKing <dario.knezovich@gmail.com>
2020-02-21 19:22:30 +0100

    added functionality for create category

--- public/js/categories/index.js
+++ public/js/categories/index.js
@@ -4,6 +4,7 @@ let api = function (method = 'GET', url, data) {
 
     let xhr = new XMLHttpRequest();
     xhr.open(method, url, false);
+    xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.setRequestHeader('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImRhcmlvLmtuZXpvdmljaEBnbWFpbC5jb20iLCJ1c2VyX2lkIjoxLCJyb2xlX2lkIjozLCJpYXQiOjE1NzkyOTcxMzV9.1SDu5PBgRP-kvVZzGuZQstKIJA6v4pR0Ev7iI4jTtak');
     xhr.onload = function () {
         if (this.status >= 200 && this.status < 300) {
@@ -28,15 +29,38 @@ let api = function (method = 'GET', url, data) {
 }
 
 var deleteBtn = document.querySelector('.delete-category');
+var createBtn = document.getElementById('create-category');
 
-deleteBtn.addEventListener("click", function (element) {
-    var id = element.target.id;
+if(deleteBtn) {
+    deleteBtn.addEventListener("click", function (element) {
+        var id = element.target.id;
 
-    let response = api('DELETE', `http://localhost:4000/api/category/${id}`);
+        let response = api('DELETE', `http://localhost:4000/api/category/${id}`);
 
-    if(!response) {
-        return
-    }
+        if(!response) {
+            return
+        }
 
-    window.location.reload();
-});
+        window.location.reload();
+    });
+}
+
+if(createBtn) {
+    createBtn.addEventListener("click", function (e) {
+        let category_name = document.getElementById('category_name').value,
+            category_description = document.getElementById('category_description').value;
+
+        let payload = {
+            category_name,
+            category_description
+        };
+
+        let response = api('POST', 'http://localhost:4000/api/category/create', JSON.stringify(payload));
+        if (!response) {
+            return
+        }
+
+        window.location.href = "/admin/categories/all";
+    });
+}
+
