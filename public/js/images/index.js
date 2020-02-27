a78b2657320f690a4005728b278d9220581f04e6
DaraKing <dario.knezovich@gmail.com>
2020-02-27 21:00:33 +0100

    bolje ti je da budes dobar prema meni ti

--- public/js/images/index.js
+++ public/js/images/index.js
@@ -10,12 +10,22 @@ if(submitBtn) {
     submitBtn.addEventListener("click", function (e) {
         e.preventDefault();
         let form = document.querySelector("form");
-        console.log(form);
 
         let formData = new FormData(form);
         let request = new XMLHttpRequest();
         request.open("POST", "http://localhost:4000/api/category-photo/upload");
         request.setRequestHeader("Authorization", `Bearer ${user.user_token}`);
+        request.onload = function () {
+            if (this.status >= 200 && this.status < 300) {
+                window.history.back();
+            } else {
+                console.warn({
+                    status: this.status,
+                    statusText: xhr.statusText,
+                    error: this.responseText,
+                });
+            }
+        };
         request.send(formData);
     });
 }
